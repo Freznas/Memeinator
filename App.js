5b8f473aa8b2341477477a70cb726221f149d656
Dennis <nilssondennis97@gmail.com>
2024-10-14 10:13:59 +0200

    skeleton

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { GenerateView } from "./GenerateView"
+import { SavedView } from "./SavedView"
+
 
 export default function App() {
   return (
