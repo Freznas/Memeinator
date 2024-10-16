b2acf2c5d407261ad147c047e45e03c4fae7b804
Mehdi <meh.tavakoli@gmail.com>
2024-10-16 09:58:51 +0200

    SavedView added

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 //Import dependencies & modules from react/react-native
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { SavedView } from "./SavedView";
+import { GenerateView } from "./GenerateView";
 //Import local modules
-import GenerateView from './GenerateView'; // Importera GenerateView
-import SavedView from './SavedView';       // Importera SavedView
+// import GenerateView from './GenerateView'; // Importera GenerateView
+// import SavedView from './SavedView';       // Importera SavedView
 
 //Create tab navigator instance
 const Tab = createBottomTabNavigator();
@@ -14,21 +15,17 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen 
-        
-          name="Generate" 
+        <Tab.Screen
+          name="Generate"
           component={GenerateView}
           options={{ headerShown: false }} // Hides the header for the "Generate"View
         />
-        <Tab.Screen 
-          name="Saved" 
-          component={SavedView} 
+        <Tab.Screen
+          name="Saved"
+          component={SavedView}
           options={{ headerShown: false }} // Hides the header for the "Saved"View
         />
       </Tab.Navigator>
     </NavigationContainer>
-    
- 
   );
 }
-
