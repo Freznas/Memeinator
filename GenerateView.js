27d0d50dc3c501ea9202698fe629836e4b490799
MaxHendessKYH <maxhendess@gmail.com>
2024-10-16 16:48:06 +0200

    implement useState for meme array

--- GenerateView.js
+++ GenerateView.js
@@ -19,7 +19,7 @@ export function GenerateView(){
     const [texts, setTexts] = useState([])
     const [textFieldsCount, setTextFieldsCount] = useState(0);
 
-    var memes = []
+    const [memes, setMemes] = useState([])
     useEffect(() =>{
         const fetchTextFieldCount = () => {
             //Antalet hämtas från API anrop
@@ -30,7 +30,7 @@ export function GenerateView(){
         }
         fetchTextFieldCount()
         fetchMemes()
-        getMemes()
+        getMemesFromAsyncStorage()
     },[]);
 
     const handleTextChange = (text, index) =>{
@@ -46,6 +46,54 @@ export function GenerateView(){
         setTexts(Array(textFieldsCount).fill(""))
     }
 
+    const saveMemeInAsyncStorage = async() =>{
+      //Generate dummy meme
+      var newMeme = {
+        "id": "61579",
+        "name": "One Does Not Simply",
+        "url": "https://i.imgflip.com/1bij.jpg",
+        "width": 568,
+        "height": 335,
+        "box_count": 2
+    }
+    //create array with new meme
+    const updatedmemes = [...memes, newMeme]
+    setMemes(updatedmemes)
+
+        try {
+            //Put array in storage
+            await AsyncStorage.setItem('memesList', JSON.stringify(updatedmemes));
+            console.log('Meme Saved!');
+            alert("Meme Saved!")
+        } catch (error) {
+            console.error('Error saving Meme:', error);
+        }
+    }
+  const  getMemesFromAsyncStorage= async() =>{
+          try {
+              //get array from storage
+              const storedMemes = await AsyncStorage.getItem('memesList');
+              if (storedMemes !== null) {
+                 setMemes(JSON.parse(storedMemes))
+                 console.log('Retrieved memes from asyncStorage:' + JSON.parse(storedMemes));
+              } else {
+                  console.log('Async Storage contains no memes');
+              }
+          } catch (error) {
+              console.error('Error retrieving memes:', error);
+          }
+  }
+  
+  const deleteAllMemes = async () =>
+  {
+          try {
+               await AsyncStorage.clear()
+              console.log("Deleted all memes")
+              setMemes([])
+           } catch (error) {
+                  console.error('Error clearing storage:', error);
+                }
+  }
     return(
         <View style={[styles.container]}>
 
@@ -101,26 +149,21 @@ export function GenerateView(){
 
               <View style={styles.buttonContainer}>
                 <Pressable style={styles.pressableStyle} 
-                onPress = { () => alert("Meme Saved!")}>
+                onPress = { () =>  saveMemeInAsyncStorage()}>
                     <Text>Save</Text>
+                    </Pressable>
+                    
+                    <Pressable style={styles.pressableStyle} 
+                onPress = { () =>  deleteAllMemes()}>
+                    <Text>Delete Storage</Text>
                     </Pressable>
                 <Pressable style={styles.pressableStyle} onPress={handleDiscard}>
                     <Text>Discard</Text>
                     </Pressable>
 
               </View>
-           
-                
-
-
         </View>
-
-
-
-
     )
-
-
 }
 
 const styles = StyleSheet.create({
@@ -212,91 +255,3 @@ const styles = StyleSheet.create({
     }
     
 })
-import { useEffect } from 'react';
-import { TouchableOpacity, StyleSheet, Text, View,Button  } from 'react-native';
-import React from "react";
-export function GenerateView()
-{
-  var memes = []
-
-//get the meme collection from storage if it exsists.
-  useEffect(() =>{
-    getMemes()
-  });
-  async function saveMeme(){
-    
-    var newMeme = {
-        "id": "61579",
-        "name": "One Does Not Simply",
-        "url": "https://i.imgflip.com/1bij.jpg",
-        "width": 568,
-        "height": 335,
-        "box_count": 2
-    }
-    memes.push(newMeme)
-
-        try {
-            await AsyncStorage.setItem('memesList', JSON.stringify(memes));
-            console.log('Data saved');
-        } catch (error) {
-            console.error('Error saving data:', error);
-        }
-    
-}
-async function getMemes()
-{
-        try {
-            const storedMemes = await AsyncStorage.getItem('memesList');
-            //handle data from storage
-            if (storedMemes !== null) {
-                const data = JSON.parse(storedMemes);
-                console.log('Retrieved data:', data);
-                memes =[... data]
-            } else {
-                console.log('thou art memeless');
-            }
-        } catch (error) {
-            console.error('Error retrieving memes:', error);
-        }
-}
-
-async function deleteAllMemes()
-{
-        try {
-             await AsyncStorage.clear()
-            console.log("Deleted all memes")
-            memes = []
-         } catch (error) {
-                console.error('Error clearing storage:', error);
-              }
-}
-return(
-    <View>
-      <TouchableOpacity style={styles.btn} onPress={() => saveMeme()}>
-        <Text style={styles.buttonText}>Save Meme </Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.btn} onPress={() => getMemes()}>
-        <Text style={styles.buttonText}>Get all Memes</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.btn} onPress={() => deleteAllMemes()}>
-        <Text style={styles.buttonText}>Meme Delete</Text>
-      </TouchableOpacity>
-    </View>
-)
-}
-const styles = StyleSheet.create({
-  btn:{
-        width: 150,
-        padding: 10,
-        margin: 10,
-        borderWidth: 1,
-        borderColor: "thistle",
-        borderRadius: 50,
-  },
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#fff",
-  },
-});
