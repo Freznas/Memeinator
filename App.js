ff8d2addfe3ad05bd3df3d823724baf491882b6c
Juhee <asmilejuliaz@gmail.com>
2024-10-21 23:47:07 +0200

    Navigationbar icon is added.

--- App.js
+++ App.js
@@ -3,6 +3,7 @@
 import * as React from "react";
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Icon from "react-native-vector-icons/MaterialIcons";
 //Import local modules
 import { GenerateView } from "./GenerateView"; // Importera GenerateView
 import { SavedView } from "./SavedView"; // Importera SavedView
@@ -22,13 +23,24 @@ export default function App() {
   return (
     <NavigationContainer theme={MyTheme}>
       <Tab.Navigator
-        screenOptions={{
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+
+            if (route.name === "Generator") {
+              iconName = "build";
+            } else if (route.name === "Your memes") {
+              iconName = "save";
+            }
+
+            return <Icon name={iconName} size={size} color={color} />
+          },
           tapBarActiveTintColor: MyTheme.colors.primary,
           tapBarInactiveTintColor: 'gray',
           tabBarStyle: {
             backgroundColor: '#C6F5FF',
           },
-          }}
+        })}
       >
         <Tab.Screen
           name="Generator"
